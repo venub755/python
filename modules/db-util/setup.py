9763d1cf7cde3f6dc0e0fa4bebdcb8b7ed82a55b
Venugopal <venub755@gmail.com>
2026-08-08 12:50:10 +0530

    Implement DbContextManager for database connection management and update Flask app to utilize it

--- modules/db-util/setup.py
+++ modules/db-util/setup.py
@@ -7,5 +7,17 @@ setuptools.setup(
     author='venub',
     license='Apache License 2.0',
     author_email='venub755@gmail.com',
-    py_modules=['dbConnect']
+    py_modules=[
+        'dbConnect', 
+        'DbContextManager'
+    ],
+    install_requires=[
+        'mysql-connector-python',
+    ],
+    classifiers=[
+        'Development Status <IP_ADDRESS> 3 - Alpha',
+        'Intended Audience <IP_ADDRESS> Developers',
+        'License <IP_ADDRESS> OSI Approved <IP_ADDRESS> Apache Software License',
+        'Programming Language <IP_ADDRESS> Python <IP_ADDRESS> 3',
+    ]
 )
--- webapps/flask-demo/vsearch4web.py
+++ webapps/flask-demo/vsearch4web.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, request, session
+import logging
+import os, sys
 from html import escape
-from vsearch import vsearch
+
+from flask import Flask, render_template, request, session
+
 from auth_check import auth_check
+from DbContextManager import DbManager
+from vsearch import vsearch
 
 app = Flask(__name__)
 app.secret_key = 'TestingFlaskDemo'  # Replace with
+app.logger.setLevel(logging.DEBUG)
+app.logger.propagate = True
+logging.basicConfig(level=logging.DEBUG, force=True)
+db_config = {}
 
-def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+def setup_db_config() -> dict:
+    global db_config
+    db_config = {
+        "host": os.environ.get("DB_HOST", "localhost"),
+        "user": os.environ.get("DB_USER", "root"),
+        "password": os.environ.get("DB_PASSWORD", ""),
+        "database": os.environ.get("DB_NAME", "vsearchlogdb"),
+        "port": int(os.environ.get("DB_PORT", 3306)),
+        "read_timeout": int(os.environ.get("DB_TIMEOUT", 10)),
+    }
+    return db_config
+
+def emit_debug(message: str) -> None:
+    print(f"[FLASK DEBUG] {message}", file=sys.stderr, flush=True)
 
 @app.route('/')
 def home() -> str:
@@ -21,8 +41,13 @@ def entry_page() -> str:
 
 @app.route('/search4', methods=['POST'])
 def search4() -> str:
+    _SQL = """insert into log
+                (phrase, letters, ip, user_agent, results)
+                VALUES (%s, %s, %s, %s, %s)"""
     the_results = str(vsearch(request.form['phrase'], request.form['letters']))
-    log_request(request, the_results)
+    db_config = setup_db_config()
+    with DbManager(db_config) as cursor:
+        cursor.execute(_SQL, (request.form['phrase'], request.form['letters'], request.remote_addr, request.headers.get('User-Agent'), the_results))
     return render_template('results.html', 
                            the_title='Search Results', 
                            phrase=request.form['phrase'], 
@@ -34,13 +59,28 @@ def search4() -> str:
 @auth_check
 def view_the_log() -> 'html':
     contents = []
+    emit_debug("Viewing log file")
+
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
     titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data=contents)
+    return render_template('viewlog.html', the_title='View File Log', the_row_titles=titles, the_data=contents)
+
+@app.route('/viewDBlog')
+@auth_check
+def view_the_DBlog() -> 'html':
+    contents = []
+    db_config = setup_db_config()
+    emit_debug(f"DB Config: {db_config}")
+    with DbManager(db_config) as cursor:
+        cursor.execute("SELECT phrase, letters, ip, user_agent, results FROM log")
+        for row in cursor.fetchall():
+            contents.append([escape(str(item)) for item in row])
+    titles = ('Phrase', 'Letters', 'IP', 'User Agent', 'Results')
+    return render_template('viewlog.html', the_title='View DB Log', the_row_titles=titles, the_data=contents)
 
 @app.route('/login')
 def login() -> 'html':
